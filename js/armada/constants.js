cbbb484f9c95eeb0e79905192c644978633e8439
Krisztián Nagy <nkrisztian89@gmail.com>
2016-11-03 23:59:56 +0100

    Changed localstorage prefix, representation of letter keys to capitals.

--- js/armada/constants.js
+++ js/armada/constants.js
@@ -13,7 +13,7 @@ define(function () {
     "use strict";
     var
             GAME_NAME = "Interstellar Armada",
-            LOCAL_STORAGE_PREFIX = "interstellarArmada_",
+            LOCAL_STORAGE_PREFIX = "armada_",
             LANGUAGE_LOCAL_STORAGE_ID = LOCAL_STORAGE_PREFIX + "language",
             VERSION_LOCAL_STORAGE_ID = LOCAL_STORAGE_PREFIX + "version";
     // constants to be accessable for all game modules
